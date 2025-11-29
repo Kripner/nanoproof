ab142efdd58ed3ca13e4a1f30b70b386ea0b8ae5
Matěj Kripner <kripnermatej@gmail.com>
2025-11-29 15:34:14 +0100

    Use the new leantree.LeanClient

--- nanoproof/search/bfs.py
+++ nanoproof/search/bfs.py
@@ -2,18 +2,19 @@ import os
 
 import torch
 from leantree import LeanProject, LeanLibrary, LeanLibraries
+from leantree.repl_adapter.server import LeanClient
 
 from nanoproof.common import compute_init, compute_cleanup, get_base_dir, print0, DummyWandb, autodetect_device_type
 from nanoproof.checkpoints import load_model, save_checkpoint
 from nanoproof.engine import Engine
 from nanoproof.data.minif2f import list_theorems, get_imports
 
-# TODO: use LeanClient
-
 source = "sft" # which checkpoint to load the model from
 model_tag = "d20" # model tag to load the model from
 device_type = "" # cuda|cpu|mps (empty => autodetect)
 base_dir = get_base_dir()
+server_address = "10.10.24.134"
+server_port = 8000
 
 device_type = autodetect_device_type() if device_type == "" else device_type
 device = torch.device(device_type)
@@ -43,9 +44,14 @@ engine = Engine(model, tokenizer)
 theorem = list_theorems("Valid")[0]
 print(theorem + "\n-----")
 
-with project.environment() as env:
+client = LeanClient(server_address, server_port)
+print(f"Connected to server at {server_address}:{server_port}")
+print(f"Server status: {client.check_status()}")
+with client.get_process() as env:
     # env.send_command("import Mathlib")
+    print("Sending imports...")
     env.send_command(lean_imports)
+    print("Starting proof...")
     branch = env.proof_from_sorry(theorem)
     print(branch.state)
     # zero, succ = branch.apply_tactic("cases n")
